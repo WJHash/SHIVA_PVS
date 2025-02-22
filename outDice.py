d925f9edc7b2f9834a131854331870ff99523f21
WANGJinghao <493658958@qq.com>
2025-02-22 17:01:33 +0800

    update batch run, output file, caculate index

--- outDice.py
+++ outDice.py
@@ -3,6 +3,7 @@ import numpy as np
 import nibabel as nib
 import scipy.spatial.distance as distance
 from sklearn.metrics import jaccard_score
+from medpy.metric.binary import dc
 
 
 def load_nii(file_path):
@@ -10,32 +11,27 @@ def load_nii(file_path):
 
 
 def dice_coefficient(gt, pred):
-    pred = (pred > 0.5).astype(np.uint8)
     intersection = np.sum(gt * pred)
     return (2. * intersection) / (np.sum(gt) + np.sum(pred) + 1e-7)
 
 
 def iou_score(gt, pred):
-    pred = (pred > 0.5).astype(np.uint8)
     return jaccard_score(gt.flatten(), pred.flatten(), average='binary')
 
 
 def recall(gt, pred):
-    pred = (pred > 0.5).astype(np.uint8)
     tp = np.sum(gt * pred)
     fn = np.sum(gt * (1 - pred))
     return tp / (tp + fn + 1e-7)
 
 
 def precision(gt, pred):
-    pred = (pred > 0.5).astype(np.uint8)
     tp = np.sum(gt * pred)
     fp = np.sum((1 - gt) * pred)
     return tp / (tp + fp + 1e-7)
 
 
 def hausdorff_distance(gt, pred):
-    pred = (pred > 0.5).astype(np.uint8)
     gt_points = np.array(np.where(gt > 0)).T
     pred_points = np.array(np.where(pred > 0)).T
 
@@ -59,11 +55,16 @@ def evaluate_metrics(gt_dir, pred_dir):
         gt_path = os.path.join(gt_dir, file)
         pred_path = os.path.join(pred_dir, file)
 
-        gt = load_nii(gt_path).astype(np.uint8)
-        pred = load_nii(pred_path.replace('.nii', '_0000.nii')).astype(np.uint8)
+        gt = load_nii(gt_path)
+        pred = load_nii(pred_path.replace('.nii', '_0000.nii'))
+
+        gt = (gt > 0).astype(np.uint8)
+        pred = (pred > 0.5).astype(np.uint8)
         pred = np.squeeze(pred)
 
-        dice = dice_coefficient(gt, pred)
+
+        # dice = dice_coefficient(gt, pred)
+        dice = dc(gt, pred)
         iou = iou_score(gt, pred)
         rec = recall(gt, pred)
         pre = precision(gt, pred)
